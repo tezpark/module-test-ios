16d4854953aab8682e4e9f9c3018aab46a6512b5
Tez Park <tez.park@sendbird.com>
2025-02-14 16:36:49 +0900

    update spm version

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AIAgentModuleTest",
-            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.1/AIAgentModuleTest.xcframework.zip",
-            checksum: "b75b19897da857add69d27feaa15f05556d549ed5024836243f412b4352c100e"
+            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.2/AIAgentModuleTest.xcframework.zip",
+            checksum: "cd97b0adbc4f95e0160d2ec1bc040b7889cfe0f376516973368e5dff4d4f3ac9"
         )
     ]
 )
