e0190ec5ceb92fea8b3fef70e60799ae6792eb0c
Tez Park <tez.park@sendbird.com>
2025-02-17 11:46:02 +0900

    107

--- Core/Package~.swift
+++ Core/Package~.swift
@@ -3,14 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "AIAgentCore",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        .library(name: "AIAgentCore", targets: ["AIAgentCore"])
+        .library(
+            name: "AIAgentCore",
+            targets: ["AIAgentCore"]
+        )
     ],
     targets: [
         .binaryTarget(
             name: "AIAgentCore",
-            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.7/AIAgentCore.xcframework.zip",
+            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.5/AIAgentCore.xcframework.zip",
             checksum: "84c895e775865bdcb6d86eaa7b9ba537c86fe131b296c4fa4a633cba439be332"
         )
     ]
--- Plugins/Package.swift
+++ Plugins/Package.swift
@@ -5,22 +5,17 @@ let package = Package(
     name: "MainModule", // for customer module, 커스텀 로직들 + 설정들 세팅한 init 제공
     platforms: [.iOS(.v13)],
     products: [
-        .library(
-            name: "MainModule",
-            targets: ["MainModule"]
-        )
+        .library(name: "MainModule", targets: ["MainModule"])
     ],
     dependencies: [
-//        .package(url: "https://github.com/tezpark/module-test-ios.git", from: "1.0.0") // 1.0.0 ≤ x < 2.0.0
-        .package(url: "https://github.com/tezpark/module-test-ios.git", "1.0.4"..."1.1.0") // 1.0.4
+        .package(url: "https://github.com/tezpark/module-test-ios/AIAgentCore.git", from: "1.0.6")
     ],
     targets: [
         .target(
             name: "MainModule",
             dependencies: [
-                .product(name: "AIAgentCore", package: "module-test-ios")
-            ], // core
-            path: "Sources/MainModule"
+                .product(name: "AIAgentCore", package: "AIAgentCore")
+            ]
         ),
     ]
 )
