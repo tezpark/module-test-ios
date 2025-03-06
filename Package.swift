8f3fa8e3bb2bcadf9e853911dc015ef7a9f5069c
Tez Park <tez.park@sendbird.com>
2025-03-06 15:38:25 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AIAgent",
+    name: "AIAgentCore",
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "AIAgentCore", targets: ["AIAgentCore"])
