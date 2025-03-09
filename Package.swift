348461e7b3a9dd0a094ae5b9baa984c1c5386e21
Tez Park <tez.park@sendbird.com>
2025-03-10 03:46:11 +0900

    1.0.14

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AIAgentCore",
-            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.13/AIAgentCore.xcframework.zip",
-            checksum: "1c90273e6edfedcaf3197e1f4f8b333bb43b6188a24df5310870679d0172a7a0"
+            url: "https://github.com/tezpark/module-test-ios/releases/download/1.0.14/AIAgentCore.xcframework.zip",
+            checksum: "f0ed68e6bb45bc0b514ed2093fb59d7790efadd02cb014e641df6b084a60aa0a"
         )
     ]
 )
